d2849d36e1cbd20240e3c88dd4b321b0413ca2d3
nrussell6 <nrussell6@knights.ucf.edu>
2020-06-19 13:07:13 -0400

    Comments & Test Cases Added
    Added comments explaining what each part of the object does.
    Added test cases. Just uncomment and run in your terminal.

--- facebookProfileObject.js
+++ facebookProfileObject.js
@@ -1,20 +1,62 @@
 var facebookProfile = {
-    userName: "",
-    friends: 0,
-    messages: [],
+    userName: "", //Your username
+    friends: 0, //how many friends you have
+    messages: [], //an empty array that'll contain your messages.
+    
+    //Changes the user name
     addUserName: function newUserName(name){
         facebookProfile.userName = name;
     },
+    
+    //Adds a string message to your messages array.
     postMessage: function addMessage(message){
         facebookProfile.messages.push(message);
     },
+    
+    //deletes a message. Specify a number for which one in the array to delete, starting at 0
     deleteMessage: function removeMessage(index){
         facebookProfile.messages.splice(index, 1);
     },
+    
+    //Increases how many friends you have
     addFriend: function friendCountIncrease(){
         facebookProfile.friends++;
     },
+    
+    //reduces how many friends you have
     removeFriend: function friendCountDecrease(){
         facebookProfile.friends--;
     }
 };
+
+/******TEST CASES*****/
+//addUserName - 
+// facebookProfile.addUserName("Nick");
+// console.log(facebookProfile.userName);
+
+//postMessage
+// facebookProfile.postMessage("Message 1");
+// facebookProfile.postMessage("Message2");
+// facebookProfile.postMessage("Message3");
+// console.log(facebookProfile.messages);
+
+//deleteMessage
+//ADDING MESSAGES ONE CAN BE DELETED
+// facebookProfile.postMessage("Message 1");
+// facebookProfile.postMessage("Message2");
+// facebookProfile.postMessage("Message3");
+// console.log(facebookProfile.messages);
+// facebookProfile.deleteMessage(1);
+// console.log(facebookProfile.messages);
+
+//addFriend
+// facebookProfile.addFriend();
+// console.log(facebookProfile.friends);
+
+//removeFriend
+// facebookProfile.addFriend();
+// facebookProfile.addFriend();
+// facebookProfile.addFriend();
+// facebookProfile.addFriend();
+// facebookProfile.removeFriend();
+// console.log(facebookProfile.friends);
